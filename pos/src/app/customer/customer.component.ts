bc9f307733aae6022da890fdf8079475f8012856
Iman Adithya <121780063+ImanAdithya@users.noreply.github.com>
2023-08-03 16:40:24 +0530

    content update

--- pos/src/app/customer/customer.component.ts
+++ pos/src/app/customer/customer.component.ts
@@ -19,17 +19,36 @@ export class CustomerComponent {
   currentEmployeID="";
 
   constructor(private http:HttpClient){
-      this.getAllCustomer();
+      this.getAllEmployee();
 
   }
 
-  getAllCustomer(){
-    this.http.get("http://localhost:8082/api/v1/employee/getAllEmployee").subscribe((resultDate:any)=>{
-      this.isResultReloaded=resultDate;
-      console.log(resultDate);
-      this.EmployeeArray=resultDate;
+  getAllEmployee(){
+    this.http.get("http://localhost:8082/api/v1/employee/getAllEmployee").subscribe((resultData:any)=>{
+      this.isResultReloaded=resultData;
+      console.log(resultData);
+      this.EmployeeArray=resultData;
     })
   }
 
+  register(){
+
+    let bodyData={
+      "name":this.name,
+      "address":this.address,
+      "mobile":this.mobile
+    };
+
+    this.http.post("http://localhost:8082/api/v1/employee/save",bodyData,{responseType:'text'}).subscribe((resultData:any)=>{
+      console.log(resultData);
+      alert("Employee Added Succuss");
+      this.getAllEmployee();
+
+
+      this.name='';
+      this.address='';
+      this.mobile=0;
+    })
+  }
 
 }
